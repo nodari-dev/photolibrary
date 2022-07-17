3f2b28900f5ea79f584315020cc9efa04e0a0375
nodaridev <nodari.pylypyshak@gmail.com>
2022-07-17 02:41:31 +0200

    implemented apollo provider, getting first photo

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
 import "./theme/style.scss";
+import {gql, useQuery} from "@apollo/client";
+
+
+const test = gql`
+    query ListPhotos {
+        listPhotos {
+            id
+            color
+            likes
+            urls {
+                full
+                raw
+                regular
+            }
+            created_at
+            links {
+                html
+            }
+            user {
+                links {
+                    html
+                }
+                id
+                first_name
+                last_name
+            }
+        }
+    }
+`;
 
 function App() {
 
-    const fetchPromise = fetch(`https://api.unsplash.com/photos/?client_id=`);
+    // const fetchPromise = fetch(`https://api.unsplash.com/photos/?client_id=`);
+    //
+    // fetchPromise
+    //     .then(response => {
+    //         return response.json();
+    //     })
+    //     .then(json => {
+    //         console.log(json);
+    //     })
+    //     .catch(error => {
+    //         console.error(error);
+    //     })
+    //     ;
+    const {loading, error, data} = useQuery(test, {
 
-    fetchPromise
-        .then(response => {
-            return response.json();
-        })
-        .then(json => {
-            console.log(json);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    return (
-        <div>
-            Hello world
-        </div>
-    );
+    });
+    if (loading) return <h2>Loading...</h2>
+    console.log(data);
+    return <img src={data.listPhotos[0].urls.regular} alt=""/>
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const client = new ApolloClient({
+
+    cache: new InMemoryCache(),
+
+    uri: "http://localhost:4000/graphql"
+
+});
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
 );
