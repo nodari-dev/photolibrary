cb51a57bab23418d4a363f11a471199f57df5668
nodaridev <nodari.pylypyshak@gmail.com>
2022-08-01 17:52:24 +0200

    fixed header

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -7,15 +7,14 @@ export default function Header(){
     const elements = useGlobalThemeElements();
 
     return(
-        <p>123</p>
-            // <header className={classes.header}>
-            //     <nav className={classes.navigation}>
-            //         <NavLink className={({ isActive }) => elements.primaryLink + (isActive ? " active" : "")}
-            //                  to={"/"}>Index</NavLink>
-            //         <Link to={"/"}><h6 className={classes.navLogo}>PhotosLibrary</h6></Link>
-            //         <NavLink className={({ isActive }) => elements.primaryLink + (isActive ? " active" : "")}
-            //                  to={"/search/"}>Search</NavLink>
-            //     </nav>
-            // </header>
+            <header className={classes.header}>
+                <nav className={classes.navigation}>
+                    <NavLink className={({ isActive }) => elements.primaryLink + (isActive ? " active" : "")}
+                             to={"/"}>Index</NavLink>
+                    <Link to={"/"}><h6 className={classes.navLogo}>PhotosLibrary</h6></Link>
+                    <NavLink className={({ isActive }) => elements.primaryLink + (isActive ? " active" : "")}
+                             to={"/search/"}>Search</NavLink>
+                </nav>
+            </header>
     )
 }
